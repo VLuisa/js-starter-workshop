22851e138051e37999037868e1ebf5f45c1f098c
pinkluna <luisavasquez@luisas-mbp.wireless.dartmouth.edu>
2016-07-06 23:39:04 -0400

    all the files'

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import './style.scss';
+const $ = require('jquery');
+
+let num = 0;
+setInterval(function*counter() {
+  num++;
+  $('#main').html(`You've been on this page for ${num} seconds`);
+}, 1000);
